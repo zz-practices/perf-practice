395ed133418470336d504de07eede7942fdde7c1
wall-e <18601254750@163.com>
2017-04-23 12:12:45 -0400

    add helper.sh

---
+++ helper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+trap "killall -9 perf" 2
+
+echo "step 1: Perf record, Hit Ctrl-C to end"
+perf record -e cpu-clock -g -p `pgrep $1` --all-user
+
+echo "step 2: Perf script"
+perf script -i perf.data &> perf.unfold
+
+echo "step 3: stackcollapse-perf.pl"
+./stackcollapse-perf.pl perf.unfold &> perf.folded
+
+echo "step 4: flamegraph.pl"
+./flamegraph.pl perf.folded > perf.svg
