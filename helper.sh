d366d856b0b0eb9fee15bf78e3b879484305b922
wall-e <18601254750@163.com>
2017-04-23 12:33:14 -0400

    auto download flamegraph tools

--- helper.sh
+++ helper.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-trap "killall -9 perf" 2
+if [ $# != 1 ] ; then 
+    echo "USAGE: $0 COMMAND" 
+    echo " e.g.: $0 redis-server" 
+    exit 1; 
+fi 
+
+trap "" 2
 
 echo "step 1: Perf record, Hit Ctrl-C to end"
 perf record -e cpu-clock -g -p `pgrep $1` --all-user
@@ -9,7 +15,15 @@ echo "step 2: Perf script"
 perf script -i perf.data &> perf.unfold
 
 echo "step 3: stackcollapse-perf.pl"
+wget https://raw.githubusercontent.com/brendangregg/FlameGraph/master/stackcollapse-perf.pl 2> /dev/null
+chmod +x stackcollapse-perf.pl
 ./stackcollapse-perf.pl perf.unfold &> perf.folded
+rm -f stackcollapse-perf.pl
 
 echo "step 4: flamegraph.pl"
+wget https://raw.githubusercontent.com/brendangregg/FlameGraph/master/flamegraph.pl 2> /dev/null
+chmod +x flamegraph.pl
 ./flamegraph.pl perf.folded > perf.svg
+rm -f flamegraph.pl
+
+echo "result: perf.svg"
